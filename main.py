63c1e3041485eb99d5f094524e98f1cee12ec141
elliottgdev <146915307+elliottgdev@users.noreply.github.com>
2025-03-27 19:48:39 +1100

    the bot is done!

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 
 import subprocess
-import asyncio
-import multiprocessing
-import io
+import sys
 
 class Client(commands.Bot):
     async def on_ready(self):
@@ -24,7 +22,7 @@ class Client(commands.Bot):
 #all minecraft server code runs here as to not hang the bot
 class Server:
     def __init__(self):
-        self.cmd = ["java", "-jar", "server/server.jar", "nogui"]
+        self.cmd = ["java", "-jar", server_jar, "nogui"]
         self.process = None
         self.logger = None
         
@@ -48,10 +46,14 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = Client(command_prefix = "!", intents=intents)
 
-#test server
-guild_id = discord.Object(id=1179627891634470995)
-#bot channel
-#channel = client.get_channel(1353657476691787857)
+bot_config = {}
+with open('botconf.txt') as file:
+    bot_config = file.readlines()
+
+api_key = bot_config[0].strip()
+guild_id = discord.Object(int(bot_config[1].strip()))
+channel = client.get_channel(int(bot_config[2].strip()))
+server_jar = bot_config[3].strip()
 
 #server the bot interacts with
 server = Server()
@@ -59,26 +61,34 @@ server = Server()
 #start server
 @client.tree.command(name="start", description="Starts server.", guild=guild_id)
 async def start_server(interaction: discord.Interaction):
-    await interaction.response.send_message('<:minecraft:1353669586356015135> Starting server')
+    await interaction.response.send_message('<:minecraft:1353669586356015135> Starting server.')
     server.start_server()
 
 #stop server / ping command
-@client.tree.command(name="stop", description="Stops pinging google", guild=guild_id)
+@client.tree.command(name="stop", description="Stops server.", guild=guild_id)
 async def stop_running(interaction:discord.Interaction):
-    await interaction.response.send_message('<:minecraft:1353669586356015135> Stopping')
+    await interaction.response.send_message('<:minecraft:1353669586356015135> Stopping server.')
     server.stop_server()
-    
-#test command
-@client.tree.command(name="print", description="Parameter Test", guild=guild_id)
-async def param_test(interaction: discord.Interaction, param: str):
-    await interaction.response.send_message(param)
 
 #command command
-@client.tree.command(name="server_command", description="Parameter Test", guild=guild_id)
+@client.tree.command(name="server_command", description="Sends server a command.", guild=guild_id)
 async def command_input(interaction: discord.Interaction, param: str):
-    await interaction.response.send_message('<:minecraft:1353669586356015135> Sending command to server')
+    await interaction.response.send_message('<:minecraft:1353669586356015135> Sending command to server.')
     server.command(param)
 
-bot_config = open('botconf.txt')
-api_key = bot_config.read()
+#move bot
+@client.tree.command(name="move", description="Moves the bot to a different channel and/or server.", guild=guild_id)
+async def command_input(interaction: discord.Interaction, new_guild: str, new_channel: str):
+    await interaction.response.send_message('<:minecraft:1353669586356015135> Sending command to server.')
+    bot_config[1] = f'{new_guild}\n'
+    bot_config[2] = f'{new_channel}\n'
+    with open('botconf.txt', 'w') as file:
+        file.writelines(bot_config)
+
+#kill bot
+@client.tree.command(name="kill", description="Kills the bot. Please close server before killing the bot", guild=guild_id)
+async def command_input(interaction: discord.Interaction):
+    await interaction.response.send_message('<:minecraft:1353669586356015135> Goodnight.')
+    sys.exit()
+
 client.run(api_key)
--- server_log.py
+++ server_log.py
@@ -2,8 +2,15 @@ import requests
 
 log = open('server.log')
 
-botconf = open('botconf.txt')
-bot_api_token = botconf.read()
+bot_config = {}
+with open('botconf.txt') as file:
+    bot_config = file.readlines()
+
+api_key = bot_config[0].strip()
+guild_id = (int(bot_config[1].strip()))
+channel = (int(bot_config[2].strip()))
+server_jar = bot_config[3].strip()
+
 
 BASE_URL = f"https://discord.com/api/v9"
 SEND_URL = BASE_URL + "/channels/{id}/messages"
@@ -11,26 +18,28 @@ DM_URL = BASE_URL + f"/users/@me/channels"
 
 
 headers = {
-    "Authorization": f"Bot {bot_api_token}",
+    "Authorization": f"Bot {api_key}",
     "User-Agent": f"DiscordBot"
 }
-
-guild_id = 1179627891634470995
-dm_channel_id = 1353657476691787857
 
 log_start = len(log.readlines())
 #r = requests.post(SEND_URL.format(id=dm_channel_id), headers=headers, json={"content": str(log_start)})
 
-a = log_start
+latest_log = log_start
 
 while True:
-    b = 0
+    index = 0
     
     log = open('server.log')
     for line in log:
-        if b >= a:
+        if index >= latest_log:
+            with open('botconf.txt') as file:
+                bot_config = file.readlines()
+            guild_id = (int(bot_config[1].strip()))
+            channel = (int(bot_config[2].strip()))
+
             #await channel.send('<a:server:1353939826243665931> ' + line.strip())
             print('<a:server:1353939826243665931> ' + line.strip())
-            r = requests.post(SEND_URL.format(id=dm_channel_id), headers=headers, json={"content": '<a:server:1353939826243665931> ' + line.strip()})
-            a += 1
-        b += 1
+            r = requests.post(SEND_URL.format(id=channel), headers=headers, json={"content": '<a:server:1353939826243665931> ' + line.strip()})
+            latest_log += 1
+        index += 1
